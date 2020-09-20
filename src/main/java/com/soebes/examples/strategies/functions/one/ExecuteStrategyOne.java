87210a4224c836882d2918ef553b5cec26b8177a
Karl Heinz Marbaise <kama@soebes.de>
2020-09-20 15:24:30 +0200

    Improved example.

--- src/main/java/com/soebes/examples/strategies/functions/one/ExecuteStrategyOne.java
+++ src/main/java/com/soebes/examples/strategies/functions/one/ExecuteStrategyOne.java
@@ -7,21 +7,20 @@ import java.util.List;
 import java.util.Map;
 
 import static java.util.function.Function.identity;
-import static java.util.stream.Collectors.toMap;
+import static java.util.stream.Collectors.toUnmodifiableMap;
 
 @Service
 public class ExecuteStrategyOne {
 
-  private Map<String, IOneStrategy> strategies;
+  private final Map<String, IOneStrategy> strategies;
 
   public ExecuteStrategyOne(List<IOneStrategy> strategies) {
     this.strategies = strategies.stream()
         .collect(
-            toMap(k -> k.getClass().getDeclaredAnnotation(Qualifier.class).value(), identity()));
+            toUnmodifiableMap(k -> k.getClass().getDeclaredAnnotation(Qualifier.class).value(), identity()));
   }
 
   public void executeStrategyOne(String name) {
-    strategies.entrySet().forEach(s -> System.out.println("k:'" + s.getKey() + "' v:" + s.getValue() + "'"));
     if (!strategies.containsKey(name)) {
       throw new IllegalArgumentException("The key " + name + " does not exist.");
     }
--- src/main/java/com/soebes/examples/strategies/functions/two/ExecuteStrategyTwo.java
+++ src/main/java/com/soebes/examples/strategies/functions/two/ExecuteStrategyTwo.java
@@ -7,20 +7,19 @@ import java.util.List;
 import java.util.Map;
 
 import static java.util.function.Function.identity;
-import static java.util.stream.Collectors.toMap;
+import static java.util.stream.Collectors.toUnmodifiableMap;
 
 @Service
 public class ExecuteStrategyTwo {
 
-  private Map<String, ITwoStrategy> strategies;
+  private final Map<String, ITwoStrategy> strategies;
 
   public ExecuteStrategyTwo(List<ITwoStrategy> strategies) {
     this.strategies = strategies.stream()
-        .collect(toMap(k -> k.getClass().getDeclaredAnnotation(Qualifier.class).value(), identity()));
+        .collect(toUnmodifiableMap(k -> k.getClass().getDeclaredAnnotation(Qualifier.class).value(), identity()));
   }
 
   public void executeStrategyOne(String name) {
-    strategies.entrySet().forEach(s -> System.out.println("k:'" + s.getKey() + "' v:" + s.getValue() + "'"));
     if (!strategies.containsKey(name)) {
       throw new IllegalArgumentException("The key " + name + " does not exist.");
     }
