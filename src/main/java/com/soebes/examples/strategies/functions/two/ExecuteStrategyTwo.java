1a71fd692d8ab1493483ad0e6dbbb1fac616c448
Karl Heinz Marbaise <kama@soebes.de>
2020-09-20 15:06:30 +0200

    Improved.

--- src/main/java/com/soebes/examples/strategies/functions/two/ExecuteStrategyTwo.java
+++ src/main/java/com/soebes/examples/strategies/functions/two/ExecuteStrategyTwo.java
@@ -1,12 +1,13 @@
 package com.soebes.examples.strategies.functions.two;
 
-import static java.util.function.Function.identity;
-import static java.util.stream.Collectors.toMap;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Service;
+
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.toMap;
 
 @Service
 public class ExecuteStrategyTwo {
@@ -15,9 +16,7 @@ public class ExecuteStrategyTwo {
 
   public ExecuteStrategyTwo(List<ITwoStrategy> strategies) {
     this.strategies = strategies.stream()
-        .collect(
-            toMap(k -> k.getClass().getDeclaredAnnotation(Qualifier.class).value(), identity()));
-
+        .collect(toMap(k -> k.getClass().getDeclaredAnnotation(Qualifier.class).value(), identity()));
   }
 
   public void executeStrategyOne(String name) {
